591b1cc23c67cc811a29c2579360949d61bc4d33
Diego <diegonicita@gmail.com>
2021-09-16 21:51:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 const morgan = require('morgan');
 
 const publicPath = path.resolve(__dirname, "./public");
