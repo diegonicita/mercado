c20a58ee4d2c52820c6436e20505d0c8ed95107c
diegonicita <diegonicita@gmail.com>
2021-09-18 16:43:33 -0300

    terminando el footer y los links

--- app.js
+++ app.js
@@ -13,5 +13,14 @@ app.listen(port, () => console.log('Server Running on port: ' + port));
 app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, "./views/login.html"))});
 app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, "./views/register.html"))});
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "./views/home.html"))});
-app.get('*', (req, res) => {res.status(404).send('404 not found. <br> ¡Houston, tenemos problemas!');})
+app.get('/error', (req, res) => {res.sendFile(path.join(__dirname, "./views/error.html"))});
+app.get('*', (req, res) => {res.sendFile(path.join(__dirname, "./views/error.html"))});
 
+// Paleta de colores
+
+// Amarillo: #EAC926;
+// Azul: #03264C;
+// Celeste: #1259c3;
+// Gris oscuro: #666;
+// Gris claro: #ebebeb;
+// Verde: #39b54a;
