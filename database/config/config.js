0cdd03ff26a785c1755f567ee67238dc821fda9e
diegonicita <diegonicita@gmail.com>
2021-11-20 14:31:43 -0300

    testing

--- database/config/config.js
+++ database/config/config.js
@@ -15,10 +15,10 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b5bca0054d4365",
+    "password": "c213d9a6",
+    "database": "heroku_ff9b331b5cb3e0d",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
