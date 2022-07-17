e8eb8ef9a5dc0a9ed660c83ec177ccfb815ed9d6
diegonicita <diegonicita@gmail.com>
2022-07-16 23:28:22 -0300

    ajustando los precios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,6 @@
 // const db = require('../database/models');
-const { Product } = require('../database/models')
+const { Product } = require('../database/models');
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
     index: (req, res) => 
@@ -8,7 +9,7 @@ const controller = {
             Product.findAll()
               .then( 
                     p => { 
-                    res.render("home", {productos: p});
+                    res.render("home", {productos: p, toThousand});
                     })
               .catch(error => res.send(error));
         },
