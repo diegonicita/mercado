8bdb3cc8cbaa5a2bf1866f489b0e56c5af31d27e
diegonicita <diegonicita@gmail.com>
2023-01-19 11:32:14 -0300

    Change Header Style

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,17 +1,70 @@
 // const db = require('../database/models');
-const { Product } = require('../database/models');
+// const { Product } = require('../database/models');
 var toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const p =
+[{
+	id: 1, 
+	titulo: 'TV Samsung', 
+	create: '1900-01-01 00:00:00', 
+	edited: null, 
+	description: "Hola",
+	eliminalbe: 1,
+	precio: 10000,
+	descuento: 10,
+	imagen: 'img-tv-samsung-smart.jpg',
+	categoria: 1
+},
+{
+	id: 2, 
+	titulo: 'MacBook Pro', 
+	create: '1900-01-01 00:00:00', 
+	edited: null, 
+	description: "Hola",
+	eliminalbe: 1,
+	precio: 250000,
+	descuento: 12,
+	imagen: 'img-macbook-pro-2019.jpg',
+	categoria: 1
+},
+{
+	id: 3, 
+	titulo: 'Cafetera Moulinex', 
+	create: '1900-01-01 00:00:00', 
+	edited: null, 
+	description: "Hola",
+	eliminalbe: 1,
+	precio: 5000,
+	descuento: 8,
+	imagen: 'img-cafetera-moulinex.jpg',
+	categoria: 1
+},
+{
+	id: 4, 
+	titulo: 'Samsung Galaxy', 
+	create: '1900-01-01 00:00:00', 
+	edited: null, 
+	description: "Hola",
+	eliminalbe: 1,
+	precio: 8000,
+	descuento: 15,
+	imagen: 'img-samsung-galaxy-s10.jpg',
+	categoria: 1
+}
+
+]
+	
 
 const controller = {
     index: (req, res) => 
         {
             res.locals.ads = true; // muestras las propagandas del header //
-            Product.findAll()
-              .then( 
-                    p => { 
-                    res.render("home", {productos: p, toThousand: toThousand });
-                    })
-              .catch(error => res.send(error));
+            //Product.findAll()
+            //  .then( 
+            //        p => { 
+            res.render("home", {productos: p, toThousand: toThousand });
+            //        })
+            //  .catch(error => res.send(error));
         },
 }
 
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,18 +1,18 @@
 
-const { Product } = require('../database/models')
+// const { Product } = require('../database/models')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
     detail: (req, res) => 
         {
-    Product.findByPk(req.params.id).then((result) => {
+   // Product.findByPk(req.params.id).then((result) => {
 
-        res.render("detail", { product: result, toThousand });
-        
-    }).catch((err) => {
-        res.send(err);
-        
-    });
+   //     res.render("detail", { product: result, toThousand });
+   //     
+   // }).catch((err) => {
+   //     res.send(err);
+   //     
+   // });
 }
 }
 
