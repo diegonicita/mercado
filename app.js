a531750c7f85565c680afc514c55850cb3781442
diegonicita <diegonicita@gmail.com>
2023-01-28 16:15:31 -0300

    Remove Bootstrap From Project

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const path = require('path')
+const app = express()
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
-// Middleware   
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
-app.use(morgan('tiny'));
+// Middleware
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
+app.use(morgan('tiny'))
 app.set('view engine', 'ejs')
 
-app.listen(process.env.PORT, () => console.log('Server Running on port: http://localhost:' + process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log('Server Running on port: http://localhost:' + process.env.PORT)
+)
 
-const mainRoutes = require('./routes/main.js');
-const userRoutes = require('./routes/user.js');
-const productsRoutes = require('./routes/products.js');
+const mainRoutes = require('./routes/main.js')
+const userRoutes = require('./routes/user.js')
+const productsRoutes = require('./routes/products.js')
 
-app.use("/", mainRoutes);
-app.use("/users", userRoutes);
-app.use("/products", productsRoutes);
+app.use('/', mainRoutes)
+app.use('/users', userRoutes)
+app.use('/products', productsRoutes)
 
 // app.use("/products", productsRoutes);
 
@@ -31,8 +33,8 @@ app.use("/products", productsRoutes);
 // app.get('/', (req, res) => {
 
 //     Product.findAll()
-//         .then( 
-//             p => { 
+//         .then(
+//             p => {
 //             res.render("home", {productos: p});
 //             })
 //         .catch(error => res.send(error));
