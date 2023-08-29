7204d11e9b55fb0daf805c99bc3b31697354bb4b
diegonicita <diegonicita@gmail.com>
2023-08-29 18:01:26 -0300

    Add cors

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const path = require('path')
+const app = express()
+// Configurar CORS
+app.use(cors())
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
-// Middleware   
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
-app.use(morgan('tiny'));
+// Middleware
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
+app.use(morgan('tiny'))
 app.set('view engine', 'ejs')
 
-app.listen(process.env.PORT, () => console.log('Server Running on port: ' + process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log('Server Running on port: ' + process.env.PORT),
+)
 
-const mainRoutes = require('./routes/main.js');
-const userRoutes = require('./routes/user.js');
-const productsRoutes = require('./routes/products.js');
-const apiRoutes = require('./routes/api.js');
+const mainRoutes = require('./routes/main.js')
+const userRoutes = require('./routes/user.js')
+const productsRoutes = require('./routes/products.js')
+const apiRoutes = require('./routes/api.js')
 
-app.use("/", mainRoutes);
-app.use("/api", apiRoutes);
-app.use("/users", userRoutes);
-app.use("/products", productsRoutes);
+app.use('/', mainRoutes)
+app.use('/api', apiRoutes)
+app.use('/users', userRoutes)
+app.use('/products', productsRoutes)
