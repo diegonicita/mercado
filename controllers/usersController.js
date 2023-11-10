a29a7997d6898fdb99aa05c759ab364e9e259e15
diegonicita <diegonicita@gmail.com>
2023-11-10 20:22:02 -0300

    Add login and JWT to project

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,56 @@
+const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const { User } = require('../database/models')
+const jwt = require('jsonwebtoken')
+
 const controller = {
-    login: (req, res) => 
-        {
-    res.render("login");
-         },
-    register: (req, res) => 
-    {
-    res.render("register");
-    },
+  login: async (req, res) => {
+    const errors = validationResult(req)
+    const emailFromBody = req.body?.email?.trim()
+    if (errors.isEmpty()) {
+      await User.findOne({
+        where: { email: emailFromBody },
+        attributes: ['id', 'username', 'password', 'email'],
+      })
+        .then((userFound) => {
+          if (
+            userFound != null &&
+            bcrypt.compareSync(req.body.password, userFound.dataValues.password)
+          ) {
+            // Create token
+            var token = jwt.sign(
+              { email: emailFromBody },
+              process.env.TOKEN_KEY,
+              {
+                expiresIn: '2h',
+              },
+            )
+            res.status(200).send({
+              error: false,
+              message: 'Login exitoso',
+              userResponse: { email: userFound.email },
+              token,
+            })
+          } else {
+            res.status(401).send({
+              error: true,
+              message: 'Las credenciales son incorrectos',
+              userResponse: {},
+              token: null,
+            })
+          }
+        })
+        .catch((error) => res.send(error))
+    } else {
+      res.json(errors)
+    }
+  },
+  register: (req, res) => {
+    res.render('register')
+  },
+  list: (req, res) => {
+    res.render('list')
+  },
 }
 
-module.exports = controller;
+module.exports = controller
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/usersController.js");
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/usersController.js')
+const verifyToken = require('../middlewares/verifyToken')
+const { body } = require('express-validator')
 
-router.get("/login", controller.login);
-router.get("/register", controller.register);
+// Validaciones login
+const loginValidations = [
+  body('email').notEmpty().withMessage('Debes completar el campo'),
+  body('password').notEmpty().withMessage('Debes completar el campo'),
+]
 
-module.exports = router;
+const registerValidations = [
+  body('password').notEmpty().withMessage('Debes completar el campo'),
+  body('email').notEmpty().isEmail().withMessage('Email invalido'),
+]
+
+router.post('/login', loginValidations, controller.login)
+router.post('/register', registerValidations, controller.register)
+router.get('/list', verifyToken, controller.list)
+
+module.exports = router
