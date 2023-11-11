6a71cf98677326f8e0a67f2090d10592e570fe85
diegonicita <diegonicita@gmail.com>
2023-11-11 20:04:11 -0300

    add register to user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 const controller = {
   login: async (req, res) => {
     const errors = validationResult(req)
+    console.log(errors)
     const emailFromBody = req.body?.email?.trim()
     if (errors.isEmpty()) {
       await User.findOne({
@@ -45,11 +46,64 @@ const controller = {
       res.json(errors)
     }
   },
-  register: (req, res) => {
-    res.render('register')
-  },
   list: (req, res) => {
     res.render('list')
+  },
+  register: async (req, res) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+      const { username, email, password } = req.body
+
+      // Check if the user with the given email already exists
+      const existingUser = await User.findOne({
+        where: { email: email.trim() },
+        attributes: ['id'],
+      })
+
+      if (existingUser) {
+        return res.status(400).json({
+          error: true,
+          message: 'El usuario con este correo electrónico ya existe',
+          userResponse: {},
+          token: null,
+        })
+      }
+
+      // If the user doesn't exist, hash the password and create a new user
+      const hashedPassword = bcrypt.hashSync(password, 10)
+
+      try {
+        const newUser = await User.create({
+          username,
+          email: email.trim(),
+          password: hashedPassword,
+        })
+
+        // Create token for the new user
+        const token = jwt.sign(
+          { email: newUser.email },
+          process.env.TOKEN_KEY,
+          { expiresIn: '2h' },
+        )
+
+        res.status(201).json({
+          error: false,
+          message: 'Registro exitoso',
+          userResponse: { email: newUser.email },
+          token,
+        })
+      } catch (error) {
+        res.status(500).json({
+          error: true,
+          message: 'Error al registrar el usuario',
+          userResponse: {},
+          token: null,
+        })
+      }
+    } else {
+      res.status(400).json(errors)
+    }
   },
 }
 
